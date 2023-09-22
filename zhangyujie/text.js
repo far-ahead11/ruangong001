27f9a565386e3f5d15cf17574373f39e85f7899f
xuexuej <1139691394@qq.com>
2023-09-22 13:53:31 +0800

    创建 text.js

---
+++ zhangyujie/text.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: xuexuej 1139691394@qq.com
+ * @Date: 2023-09-22 13:44:34
+ * @LastEditors: xuexuej 1139691394@qq.com
+ * @LastEditTime: 2023-09-22 13:45:23
+ * @FilePath: \ruangong001\zhangyujie\text.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+console.log("hello world");
